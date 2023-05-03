91a12311d38b4d037493edd285f7ff171fa09d05
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-05-03 23:55:56 +0200

    Changed accesser

--- test/test.cs
+++ test/test.cs
@@ -6,21 +6,21 @@ namespace TestJSON
         [TestMethod]
         public void onlyValue()
         {
-            Assert.AreEqual(JSON.Parser.parse("45").get().Long, 45);
-            Assert.AreEqual(JSON.Parser.parse("-4445").get().Long, -4445);
+            Assert.AreEqual(JSON.Parser.parse("45").get_number(), 45);
+            Assert.AreEqual(JSON.Parser.parse("-4445").get_number(), -4445);
 
-            Assert.AreEqual(JSON.Parser.parse("45.453").get().Double, 45.453);
-            Assert.AreEqual(JSON.Parser.parse("-945.453").get().Double, -945.453);
+            Assert.AreEqual(JSON.Parser.parse("45.453").get_number(), 45.453);
+            Assert.AreEqual(JSON.Parser.parse("-945.453").get_number(), -945.453);
 
-            Assert.AreEqual(JSON.Parser.parse("true").get().Bool, true);
-            Assert.AreEqual(JSON.Parser.parse("null").get().Null, true);
+            Assert.AreEqual(JSON.Parser.parse("true").get_bool(), true);
+            Assert.AreEqual(JSON.Parser.parse("null").get(), true);
         }
 
         [TestMethod]
         public void onlyMultivariable()
         {
-            Assert.AreEqual(JSON.Parser.parse("[ 34  ]").get(0).get().Long, 34);
-            Assert.AreEqual(JSON.Parser.parse("[  34, { \"test\": \"ok\", \"num\": 98  }  ]").get(1).get("test").get().String, "ok");
+            Assert.AreEqual(JSON.Parser.parse("[ 34  ]").get(0).get_number(), 34);
+            Assert.AreEqual(JSON.Parser.parse("[  34, { \"test\": \"ok\", \"num\": 98  }  ]").get(1).get("test").get_string(), "ok");
         }
     }
 }
